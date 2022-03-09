3eb2668750c97ee560e531057917ac1a73ccb577
Achint <ag.achint@gmail.com>
2022-03-10 03:50:33 +0530

    added delete Note

--- server/dao/NotesDAO.js
+++ server/dao/NotesDAO.js
@@ -25,4 +25,33 @@ function insertNote(rollNo, title, content, tags){
   });
 }
 
-module.exports = { insertNote };
+function deleteNote(noteID){
+  return new Promise((resolve, reject) => {
+    Note.findOneAndDelete({
+      _id: noteID
+    }, 
+    function(err, note){
+      if(err){
+        reject({
+          status: 500,
+          response: "A server error occurred"
+        });
+      }
+      else if(!note){
+        reject({
+          status: 404,
+          response: "Note not found"
+        });
+      }
+      else{
+        resolve({
+          status: 202,
+          response: "Note successfully deleted"
+        });
+      }
+    });
+  });
+}
+
+
+module.exports = {insertNote, deleteNote};
