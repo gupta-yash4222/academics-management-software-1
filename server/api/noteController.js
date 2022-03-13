3809c083e39cebf0c12bddc8481db08794840264
Achint Agrawal <achint@pop-os.localdomain>
2022-03-13 21:57:09 +0530

    added update note

--- server/api/noteController.js
+++ server/api/noteController.js
@@ -1,4 +1,4 @@
-const {insertNote, deleteNote} = require('../dao/NotesDAO.js');
+const {insertNote, deleteNote, updateNote} = require('../dao/NotesDAO.js');
 
 function apiCreateNote(req, res) {
     var content = req.body.content,
@@ -28,5 +28,24 @@ function apiDeleteNote(req, res) {
     });
 }
 
+function apiUpdateNote(req, res){
+    var noteID = req.params.noteID,
+        content = req.body.content,
+        rollNo = req.body.rollNo,
+        title = req.body.title,
+        tags = req.body.tags;
+    if(!noteID ||!title || !rollNo)
+        res.status(422).json("A required field is empty");
+    else {
+        updateNote(noteID, rollNo, title, content, tags)
+        .then(result => {
+            res.status(result.status).json(result.response);
+        })
+        .catch(error => {
+            res.status(error.status).json(error.response);
+        });
+    }
+}
 
-module.exports = {apiCreateNote, apiDeleteNote};
+
+module.exports = {apiCreateNote, apiDeleteNote, apiUpdateNote};
--- server/routers/notesRouter.js
+++ server/routers/notesRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-const {apiCreateNote, apiDeleteNote} = require('../api/noteController.js');
+const {apiCreateNote, apiDeleteNote, apiUpdateNote} = require('../api/noteController.js');
 
 var router = express.Router();
 
 router.post('/', apiCreateNote);
 router.delete('/:noteID', apiDeleteNote);
+router.patch('/:noteID', apiUpdateNote);
 
 module.exports = router;
