febe23725d420c6d30294f18d84ed472335ced16
sachin thakan <sachinthakan001@gmail.com>
2022-03-23 17:20:31 +0530

    debugged create note form

--- server/api/noteController.js
+++ server/api/noteController.js
@@ -1,10 +1,14 @@
 const {insertNote, deleteNote, updateNote} = require('../dao/NotesDAO.js');
 
 function apiCreateNote(req, res) {
+    
+    req.body.rollNo = 180639;
     var content = req.body.content,
         rollNo = req.body.rollNo,
         title = req.body.title,
         tags = req.body.tags;
+
+        console.log(req.body);
     if(!title || !rollNo)
         res.status(422).json("A required field is empty");
     else {  
