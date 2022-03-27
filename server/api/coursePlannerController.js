94efbb21a6ceb78d4d2787e5e1b09af2f93850a7
Yash Gupta <yashbg7@gmail.com>
2022-03-27 15:54:11 +0530

    Add coursePlannerController

---
+++ server/api/coursePlannerController.js
@@ -0,0 +1,18 @@
+const { addCompletedCourse } = require('../dao/coursePlannerDAO')
+
+async function apiAddCompletedCourse(req, res) {
+    const courseID = req.body.courseID,
+        courseName = req.body.courseName,
+        semNumber = req.body.semNumber,
+        username = req.body.username;
+
+    addCompletedCourse(courseID, courseName, semNumber, username)
+        .then(result => {
+            return res.status(restult.status).json({ message: result.message });
+        })
+        .catch(error => {
+            return res.status(error.status).json({ message: error.message });
+        });
+}
+
+module.exports = { apiAddCompletedCourse };
