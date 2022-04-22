83914ea81d6764f8210dfcdce6308078caebc3ab
Yash Gupta <yashbg7@gmail.com>
2022-04-22 09:53:32 +0530

    Change profile page

--- my-app/src/pages/Profile.js
+++ my-app/src/pages/Profile.js
@@ -1,27 +1,51 @@
 import React from 'react';
+import axios from 'axios';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import { useState, useEffect } from 'react';
 
 const Profile = () => {
+    const [uname, setUname] = useState(null);
+    const [dept, setDept] = useState(null);
+    const [rno, setRno] = useState(null);
+    const [semnum, setSemnum] = useState(null);
+    const [name, setName] = useState(null);
+    useEffect(() => {
+        axios.get('http://localhost:3000/getUserDetails')
+            .then(res => {
+                console.log(res.message);
+                const user = res.data.user;
+                setUname(user.username);
+                setName(user.name);
+                setDept(user.department);
+                setRno(user.rollNo);
+                setSemnum(user.coursePlan.sems.length);
+            })
+            .catch(err => {
+                alert(err.message);
+            });
+    }, []);
     return (
+        // {dept && rno && semnum && name &&
         <div>
-            <Card style={{ width: '40rem' , margin:"auto" , padding:"10px", marginTop:"100px", color:"black"}}>
-                <Card.Img src="https://i.pravatar.cc/300" style={{borderRadius:"50%", float:"centre", marginLeft:"210px", width: "35%", height: "35%" }} />
+            <Card style={{ width: '40rem', margin: "auto", padding: "10px", marginTop: "100px", color: "black" }}>
+                <Card.Img src="https://i.pravatar.cc/300" style={{ borderRadius: "50%", float: "centre", marginLeft: "210px", width: "35%", height: "35%" }} />
                 <Card.Body>
-                    <Card.Title>Student Name</Card.Title>
+                    <Card.Title>{name}</Card.Title>
                     <Card.Text>
                         <ul>
-                            <li>Roll No: 200000</li>
-                            <li>Program: B.tech</li>
-                            <li>Department: XYZ</li>
-                            <li>Sem completed: 6</li>
-                            <li>Course Credits completed: 354</li>
+                            <li>Username: {uname}</li>
+                            <li>Roll No: {rno}</li>
+                            <li>Program: BTech</li>
+                            <li>Department: {dept}</li>
+                            <li>Sem completed: {semnum}</li>
                         </ul>
                     </Card.Text>
-                    <Button variant="primary">Progress Dashboard</Button>
+                    {/* <Button variant="primary">Progress Dashboard</Button> */}
                 </Card.Body>
             </Card>
         </div>
+        // }
     );
 };
 
